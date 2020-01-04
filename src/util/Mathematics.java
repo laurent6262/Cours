24ee8dfa54a3710c74eeeada3a1b3eafd7a96db0
laurent6262 <laurentfebvin@gmail.com>
2020-01-04 19:29:20 +0100

    initial commit

---
+++ src/util/Mathematics.java
@@ -0,0 +1,17 @@
+package util;
+
+final public class Mathematics {
+	
+	
+	
+	final public static Double add(double a, double b) {
+		return a+b;
+	}
+	
+	
+	
+	final public static Double sub(double a, double b) {
+		return a-b;
+	}
+
+}
